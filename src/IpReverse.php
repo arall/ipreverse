39649a05d701c43d8677d645cf43bb5ace4f6176
Víctor Montalà <victormontala@gmail.com>
2015-03-06 18:33:11 +0100

    Log deletion, changed quotes in example to properly see line breaks in linux console and added reverse lookup parsing logic for hurricane and bing

--- src/IpReverse.php
+++ src/IpReverse.php
@@ -34,7 +34,7 @@ class IpReverse
             $this->ip = $ip;
 
             // Run
-            $this->execute();
+            $this->execute('hurricane');
         }
 
         // Invalid domain
@@ -44,26 +44,81 @@ class IpReverse
     }
 
     /**
-     * Query resolver server
+     * Query resolver switcher
      *
-     * @throws ResolveErrorException If the resolver doesn't response
      * @return bool
      */
-    private function execute()
+    private function execute($server)
     {
         try {
-            $url = 'http://bgp.he.net/ip/'.$this->ip.'#_dns';
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL,               $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER,    true);
             curl_setopt($ch, CURLOPT_USERAGENT,         'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36');
             curl_setopt($ch, CURLOPT_AUTOREFERER,       true);
-            $result = curl_exec($ch);
-
-            file_put_contents('log.txt', $result);
-
+            switch ($server) {
+                case 'hurricane':
+                        $this->executeHurricane($ch);
+                    break;
+                default:
+                        $this->executeBing($ch);
+                    break;
+            }
         } catch (ResolveErrorException $e) {
             return false;
+        }
+        return true;
+    }
+
+    /**
+     * Hurricane query resolver server 
+     *
+     * @throws ResolveErrorException If the resolver doesn't response
+     */
+    private function executeHurricane($ch)
+    {
+        $url = 'http://bgp.he.net/ip/'.$this->ip.'#_dns';
+        curl_setopt($ch, CURLOPT_URL,               $url);
+        $result = curl_exec($ch);
+
+        $this->hosts = array();
+
+        $doc = new \DomDocument;
+        if($doc->loadHTML($result)) {
+            foreach($doc->getElementById('dns')->getElementsByTagName('a') as $element) {
+                if(preg_match('/^\w+\.[a-z]+$/', $element->textContent)) {
+                    $this->hosts[$element->textContent] = $element->textContent;
+                }
+            }
+        } else {
+            throw new ResolveErrorException;
+        }
+    }
+
+    /**
+     * Bing query resolver server
+     *
+     * @throws ResolveErrorException If the resolver doesn't response
+     */
+    private function executeBing($ch)
+    {
+        $url = 'http://www.bing.com/search?q=ip%3a'.$this->ip;
+        curl_setopt($ch, CURLOPT_URL,               $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,        array("Cookie: SRCHHPGUSR=NRSLT=500"));
+        $result = curl_exec($ch);
+
+        $this->hosts = array();
+
+        $doc = new \DomDocument;
+        if($doc->loadHTML($result)) {
+            foreach($doc->getElementById('b_results')->getElementsByTagName('li') as $elements) {
+                foreach($elements->getElementsByTagName('cite') as $element) {
+                    if(preg_match('/((\w+\.)?[a-zA-Z0-9\-]+\.[a-z]+)/', $element->textContent, $matches)) {
+                        $this->hosts[$matches[1]] = $matches[1];
+                    }
+                }
+            }
+        } else {
+            throw new ResolveErrorException;
         }
     }
 }
